524e7cdd1aec6777a382f4aede28701a14753506
yeeYash <thakareyash99258@gmail.com>
2025-04-23 15:13:49 +0530

    Added Script for Pipe Movement

---
+++ Assets/pipescript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class pipescript : MonoBehaviour
+{
+
+    public float movespeed = 5;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += (Vector3.left * movespeed) * Time.deltaTime;
+    }
+}
