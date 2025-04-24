fd1e1998e4ed829dd5d43a629cec70b08c273398
yeeYash <thakareyash99258@gmail.com>
2025-04-24 13:19:13 +0530

    Finished.

--- Assets/birdscript.cs
+++ Assets/birdscript.cs
@@ -5,19 +5,27 @@ public class birdscript : MonoBehaviour
 
     public Rigidbody2D myRigidBody;
     public float flapstrength;
+    public logicscript logic;
+    public bool birdisalive = true;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        gameObject.name = "Barb";
+        logic = GameObject.FindGameObjectWithTag("logic").GetComponent<logicscript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && (birdisalive == true))
         {
             myRigidBody.linearVelocity = Vector2.up * flapstrength;
         }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameover();
+        birdisalive = false;
     }
 }
--- Assets/logicscript.cs
+++ Assets/logicscript.cs
@@ -1,15 +1,27 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class logicscript : MonoBehaviour
 {
     public int score;
     public Text scoreText;
+    public GameObject GameOverscreen;
 
     [ContextMenu("Increase score")]
     public void addscore(int scoree)
     {
         score += scoree;
         scoreText.text = score.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameover()
+    {
+        GameOverscreen.SetActive(true);
     }
 }
